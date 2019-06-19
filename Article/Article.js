0188486819dbfaaf95e70235392735a67b8a8e18
gisellehargrove <gisellehargrove@gmail.com>
2019-06-19 15:36:41 -0400

    added event listener to expand button

--- Article/Article.js
+++ Article/Article.js
@@ -3,13 +3,13 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.domElement = domElement;
+    // create a reference to the ".expandButton" class.
+    this.expandButton = document.getElementsByClassName('expandButton')[0];
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle);
   }
 
   expandArticle() {
@@ -18,9 +18,9 @@ class Article {
   }
 }
 
-/* START HERE: 
+/* START HERE:
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable.
 
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
